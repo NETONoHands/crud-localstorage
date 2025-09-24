2b252fcf7e691428e62f0e3aed172931fd0165ea
Neto Trindade <neto.trindade@live.com>
2025-09-24 10:57:05 -0300

    crud quase completo

--- main.js
+++ main.js
@@ -9,13 +9,6 @@ const closeModal = () => {
     .classList.remove('active')
 }
 
-const clienteTemp = {
-    nome: "Pedro",
-    email: "peu@gmail.com",
-    celular: "11123459876",
-    cidade: "São Roque"
-}
-
 const getLocalStorage = () => JSON.parse(localStorage.getItem('db-client')) ?? [];
 
 const setLocalStorage = (dbClient) => localStorage.setItem("db-client", JSON.stringify(dbClient));
@@ -57,10 +50,74 @@ const saveCliente = () => {
             celular: document.getElementById('celular').value,
             cidade: document.getElementById('cidade').value
         }
-        criaCliente(cliente);
-        closeModal();
+        const index = document.getElementById('nome').dataset.index;
+
+        if (index == 'novo') {
+            criaCliente(cliente);
+            updateTabela()
+            closeModal();
+        } else {
+            updateClient(index, cliente);
+            updateTabela();
+            closeModal();
+        }
     }
 }
+
+const createRow = (cliente, index) => {
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+    <td>${cliente.nome}</td>
+    <td>${cliente.email}</td>
+    <td>${cliente.celular}</td>
+    <td>${cliente.cidade}</td>
+    <td>
+        <button type="button" class="button green" id="editar-${index}">Editar</button>
+        <button type="button" class="button red" id="deletar-${index}">Excluir</button>    
+    </td>
+    `
+
+    document.querySelector('#tbCliente>tbody').appendChild(newRow);
+}
+
+const clearTabela = () => {
+    const rows = document.querySelectorAll('#tbCliente>tbody tr');
+    rows.forEach(row => row.parentElement.removeChild(row));
+}
+
+const updateTabela = () => {
+    clearTabela();
+    const dbClient = readClient();
+    dbClient.forEach(createRow);
+}
+
+const preencheCampos = (cliente) => {
+    document.getElementById('nome').value = cliente.nome;
+    document.getElementById('email').value = cliente.email;
+    document.getElementById('celular').value = cliente.celular;
+    document.getElementById('cidade').value = cliente.cidade;
+    document.getElementById('nome').dataset.index = cliente.index
+}
+
+const editaCliente = (index) => {
+    const cliente = readClient()[index]
+    preencheCampos(cliente);
+    openModal();
+}
+
+const editDelete = (event) => {
+    if (event.target.type == 'button') {
+
+        const [action, index] = event.target.id.split('-');
+        if (action == 'editar') {
+            editaCliente(index);
+        } else {
+            console.log("deletando");
+        }
+    }
+}
+
+updateTabela()
 
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
@@ -70,3 +127,6 @@ document.getElementById('modalClose')
 
 document.getElementById('salvar')
     .addEventListener('click', saveCliente)
+
+document.querySelector('#tbCliente>tbody')
+    .addEventListener('click', editDelete)
